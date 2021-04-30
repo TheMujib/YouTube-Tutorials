eee926a238ab5efea41e892ebc1cf3bf52f1ccf9
Mujib Sayyad <63368252+TheMujib@users.noreply.github.com>
2021-04-30 11:01:37 +0530

    Add files via upload

---
+++ DropDown-Menu/script.js
@@ -0,0 +1,6 @@
+const myDrop = document.getElementById('myDropdown');
+const dropDown = document.querySelector('.dropdownn');
+
+dropDown.addEventListener('click', () => {
+  myDrop.classList.toggle('show');
+});
